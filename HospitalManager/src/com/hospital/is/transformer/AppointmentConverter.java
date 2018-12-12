d6d598b7c3b77abd50957ee4ac13fafdfa519050
shaimaa99 <alguetati@gmail.com>
2018-12-12 08:28:30 +0000

    Commit

--- HospitalManager/src/com/hospital/is/transformer/AppointmentConverter.java
+++ HospitalManager/src/com/hospital/is/transformer/AppointmentConverter.java
@@ -1,11 +1,16 @@
 package com.hospital.is.transformer;
 
+import java.security.KeyStore.Entry;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.hospital.is.entity.Appointment;
 import com.hospital.is.model.AppointmentDTO;
 
 public class AppointmentConverter {
+	
 
-	AppointmentDTO toDTO(Appointment appointment) {
+	public AppointmentDTO toDTO(Appointment appointment) {
 		AppointmentDTO dto = new AppointmentDTO();
 
 		dto.setDateTimeAppointment(appointment.getDateTimeAppointment());
@@ -16,7 +21,7 @@ public class AppointmentConverter {
 		return dto;
 	}
 
-	Appointment toEntity(AppointmentDTO dto) {
+	public Appointment toEntity(AppointmentDTO dto) {
 		Appointment appointment = new Appointment();
 
 		appointment.setDateTimeAppointment(dto.getDateTimeAppointment());
@@ -25,5 +30,16 @@ public class AppointmentConverter {
 		
 
 		return appointment;
+	}
+	public Map<String,AppointmentDTO> toMapDTO(Map<Integer,Appointment> appointment) {
+		Map<Integer,AppointmentDTO> dtoMap=new HashMap();
+		for(Entry<Integer,Appointment> entry : appointment.entrySet()) {
+			dtoMap.put(entry.getKey(),toDTO(entry.getValue()));
+		}
+		 return dtoMap;
+
+	}
+	public Appointment toMapEntity(AppointmentDTO dto) {
+		
 	}
 }
