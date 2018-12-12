25df4653f16d5673efc7f55e264644dc7bf87ced
shaimaa99 <alguetati@gmail.com>
2018-12-12 09:10:05 +0000

    Update PrescriptionConverter Class

--- HospitalManager/src/com/hospital/is/transformer/PrescriptionConverter.java
+++ HospitalManager/src/com/hospital/is/transformer/PrescriptionConverter.java
@@ -1,6 +1,12 @@
 package com.hospital.is.transformer;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.hospital.is.entity.Doctor;
 import com.hospital.is.entity.Prescription;
+import com.hospital.is.model.DoctorDTO;
 import com.hospital.is.model.PrescriptionDTO;
 
 public class PrescriptionConverter {
@@ -24,6 +30,25 @@ public class PrescriptionConverter {
 		
 
 		return prescription;
+	}
+	public Map<String,PrescriptionDTO> toMapDTO(Map<String,Prescription> prescriptionMap) {
+		Map<String,PrescriptionDTO> prescriptiondto=new HashMap<>();
+		for(Entry<String,Prescription> entry : prescriptionMap.entrySet()) {
+			prescriptiondto.put(entry.getKey(),toDTO(entry.getValue()));
+		}
+		 return prescriptiondto;
+
+	}
+	
+	//Ajouter toMapEntity
+		
+	public Map<String,Prescription> toMapEntity(Map<String,PrescriptionDTO> prescriptiondto) {
+		Map<String,Prescription> prescriptionMap=new HashMap<>();
+		for(Entry<String, PrescriptionDTO> entry : prescriptiondto.entrySet()) {
+			prescriptionMap.put(entry.getKey(),toEntity(entry.getValue()));
+		}
+		 return prescriptionMap;
+		
 	}
 
 }
