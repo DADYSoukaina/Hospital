6b44a7c7d88fe87d3c5109887b2d3c80be08fef5
Dikra Sordo <dikrasordo@gmail.com>
2018-12-12 11:08:03 +0100

    merge

--- HospitalManager/src/com/hospital/is/transformer/MedicationConverter.java
+++ HospitalManager/src/com/hospital/is/transformer/MedicationConverter.java
@@ -1,7 +1,13 @@
 package com.hospital.is.transformer;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.hospital.is.entity.Medication;
+import com.hospital.is.entity.Patient;
 import com.hospital.is.model.MedicationDTO;
+import com.hospital.is.model.PatientDTO;
 
 public class MedicationConverter {
 	public MedicationDTO toDTO(Medication medication) {
@@ -12,9 +18,9 @@ public class MedicationConverter {
 		dto.setDurationOfUse(medication.getDurationOfUse());
 		dto.setInstrutionOfUse(medication.getInstrutionOfUse());
 
-
 		return dto;
 	}
+
 //entity
 	public Medication toEntity(MedicationDTO dto) {
 		Medication medication = new Medication();
@@ -24,6 +30,19 @@ public class MedicationConverter {
 		medication.setDurationOfUse(dto.getDurationOfUse());
 		medication.setInstrutionOfUse(dto.getInstrutionOfUse());
 		return medication;
+	}
+
+	public Map<String, MedicationDTO> toMapDTO(Map<String, Medication> mapMedication) {
+
+		Map<String, MedicationDTO> medicationDTOMap = new HashMap<>();
+
+		for (Entry<String, Medication> entry : mapMedication.entrySet()) {
+			medicationDTOMap.put(entry.getKey(), toDTO(entry.getValue()));
+		}
+
+		// comment 2 cytfu
+
+		return medicationDTOMap;
 	}
 
 }
